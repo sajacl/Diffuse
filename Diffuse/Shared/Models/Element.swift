80353470a6ffe7e4262c2629dfefc2da8efd2967
Sajad Vishkai <sajad.vishkai@schibsted.com>
2023-02-16 11:22:34 +0100

    Nested Element and marked it as public in favour of compiler inlinable opt.

--- Diffuse/Shared/Models/Element.swift
+++ Diffuse/Shared/Models/Element.swift
@@ -2,15 +2,22 @@
 //  Copyright © FINN.no AS, Inc. All rights reserved.
 //
 
-struct Element<T: Hashable>: Hashable {
-    let value: T
-    let index: Int
-
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(value.hashValue)
-    }
-
-    static func == (lhs: Element<T>, rhs: Element<T>) -> Bool {
-        return lhs.value == rhs.value
+extension Diffuse {
+    public struct Element<T: Hashable>: Hashable {
+        public let value: T
+        public let index: Int
+        
+        public init(value: T, index: Int) {
+            self.value = value
+            self.index = index
+        }
+        
+        public func hash(into hasher: inout Hasher) {
+            hasher.combine(value.hashValue)
+        }
+        
+        public static func == (lhs: Element<T>, rhs: Element<T>) -> Bool {
+            return lhs.value == rhs.value
+        }
     }
 }
